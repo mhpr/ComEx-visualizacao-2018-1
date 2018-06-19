ca1376c56b09ca61248f5fb79286c1b878f04c2a
mhpr <mhpr@cin.ufpe.br>
2018-06-18 21:49:13 -0300

    Tratamento de dados Exportacao

--- createDatabase.py
+++ createDatabase.py
@@ -1,5 +1,8 @@
-from pymongo import MongoClient
+#from pymongo import MongoClient
 import sys
+import csv
+
+
 
 class MongoApi:
     def __init__(self, db, collection, ip='127.0.0.1', port=27017): 
@@ -25,8 +28,77 @@ class MongoApi:
 
     def isOnDb(self, key, value):
         status = False
-        for respo in self.collection.find({key : value}).limit(1): #
+        for respo in self.collection.find({key : value}).limit(1): 
             status = True
 
         return status
 
+if __name__ == "__main__":
+    uf = None
+    municipios = None
+    ncm = None
+    exportacao = None
+
+    uf = open('csvData\\UF.csv','r') 
+    estados = csv.DictReader(uf, delimiter=';')
+
+    mun = open('csvData\\VIA.csv','r')
+    vias = csv.DictReader(mun, delimiter=';')
+
+    ncm1 = open('csvData\\NCM.csv','r')
+    ncm = csv.DictReader(ncm1, delimiter=';')
+
+    pais = open('csvData\\PAIS.csv','r')
+    country = csv.DictReader(pais, delimiter=';')
+
+    exp = open('csvData\\EXP_2017.csv', 'r')
+    exportacao = csv.DictReader(exp, delimiter=';')
+
+    unit = open('csvData\\NCM_UNIDADE.csv', 'r')
+    unidade = csv.DictReader(unit, delimiter=';')
+
+    data=[]
+
+    for i in exportacao:
+        auxHash = {}
+        for j in estados:
+            if i['CO_UF'] == j['CO_UF']:
+                auxHash['SIGLA'] = j['SG_UF']
+                auxHash['ESTADO'] = j['NO_UF']   
+                break
+
+        for k in ncm:
+            if i['CO_NCM'] == k['CO_NCM']:
+                auxHash['PRODUTO'] = k['NO_NCM_POR']
+                auxHash['SUBSET'] = k['CO_EXP_SUBSET']
+                auxHash['PPE'] = k['CO_PPE']
+                break
+
+        for l in vias:
+            if i['CO_VIA'] == l['CO_VIA']:
+                auxHash['VIA'] = l['NO_VIA']
+                break
+
+        for m in country:
+            if i['CO_PAIS'] == m['CO_PAIS']:
+                auxHash['PAIS_PT'] = m['NO_PAIS']
+                auxHash['PAIS_EN'] = m['NO_PAIS_ING']
+                break
+        
+        for n in unidade:
+            if i['CO_UNID']==n['CO_UNID']:
+                auxHash['NOME_UNIDADE'] = n['NO_UNID']
+                auxHash['SIGLAS_UNIDADE'] = n['SG_UNID']
+                break
+        
+        auxHash['ANO'] = i['CO_ANO']
+        auxHash['MES'] = i['CO_MES']
+        auxHash['QT_ESTAT'] = i['QT_ESTAT']
+        auxHash['PESO_LIQUIDO_KG'] = i['KG_LIQUIDO']
+        auxHash['VALOR'] = i['VL_FOB']
+
+        data.append(auxHash)
+
+    print(data[0])
+    print(data[1])
+    
