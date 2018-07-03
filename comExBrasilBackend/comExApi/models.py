d944756aa7a7e1e14fe8068d1d5533e566703a9c
PedroEsposito <pegs@cin.ufpe.br>
2018-07-03 02:11:42 -0300

    minor change

--- comExBrasilBackend/comExApi/models.py
+++ comExBrasilBackend/comExApi/models.py
@@ -37,15 +37,43 @@ class Api(object):
             match["$match"] = {"$and": filter}
         return match
 
-    def trataMes(mes,paises):
+    def trataMes(mes,paises,topPaises):
         for pais in paises:
-            if not pais in mes:
+            if (not pais in mes) and (not pais in topPaises):
                 mes[pais] = 0
         return mes
 
+
     @classmethod
     def getStreamMap(cls, params ,imp=False, **kwargs):
-        print("aqui")
+        collection = cls.importacao_collection if imp else cls.exportacao_collection
+        paises = collection.distinct("PAIS_PT")
+        topPaises = []
+        tpa = False
+        if "tpr" or "tpa" in params:
+            pipe0 = []
+            pipe0.append({"$group" :{"_id": "$PAIS_PT", "valorTotal": {"$sum":"$VALOR"}, "quantidade": {"$sum":1} }})
+            pipe0.append({"$sort":{"valorTotal":-1}})
+            pointer0 = collection.aggregate(pipe0)
+            i = 0
+        if "tpr" in params:
+            for line in pointer0:
+                topPaises.append(line['_id'])
+                i = i +1
+                if i >= params["tpr"]:
+                    break
+            for pais in topPaises:
+                paises.remove(pais)
+        elif "tpa" in params:
+            tpa = True
+            for line in pointer0:
+                topPaises.append(line['_id'])
+                i = i +1
+                if i >= params["tpa"]:
+                    break
+            topPaises.append("outros")
+            paises = topPaises
+
         pipe = []
         match = cls.buildPipe(params)
         if match:
@@ -53,12 +81,9 @@ class Api(object):
 
         #pipe.append({"$group":{"_id":{ "pais": "$PAIS_PT", "mes":"$MES"},"valorTotal": {"$sum":"$VALOR"}, "valorMedio": { "$avg": "$VALOR" }, "quantidade": {"$sum":1} }})
         pipe.append({"$group":{"_id":{ "pais": "$PAIS_PT", "mes":"$MES"},"valorTotal": {"$sum":"$VALOR"} }})
-        pipe.append({"$sort":{"_id.mes":1}})
-        print(pipe)
-        collection = cls.importacao_collection if imp else cls.exportacao_collection
+        pipe.append({"$sort":{"_id.mes":1}})        
         pointer = collection.aggregate(pipe)
-
-        paises = collection.distinct("PAIS_PT")
+        
         data = []
         old = 1
         mes = {}
@@ -66,11 +91,19 @@ class Api(object):
         for line in pointer:
             current = line["_id"]["mes"]
             if old != current:
-                mes = cls.trataMes(mes,paises)
+                mes = cls.trataMes(mes,paises,topPaises)
                 data.append(mes)
                 mes = {}
                 mes["mes"] = current
-            mes[line["_id"]["pais"]] = line["valorTotal"]
+            if tpa:
+                if not (line["_id"]["pais"] in topPaises):
+                    if not "outros" in mes :
+                        mes["outros"] = 0
+                    mes["outros"] += line["valorTotal"] / 100000
+                else:
+                    mes[line["_id"]["pais"]] = line["valorTotal"] / 100000
+            elif not (line["_id"]["pais"] in topPaises):
+                mes[line["_id"]["pais"]] = line["valorTotal"] / 100000
             old = current
         if mes:
             data.append(mes)
